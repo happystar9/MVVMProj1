068447fa70b10439bdc00d5b5161f47183a452ef
happy <ricardo_ruizjr@hotmail.com>
2025-01-15 12:46:11 -0700

    chessboard

--- MVVMProj/MVVMProj/Pages/MainPage.xaml.cs
+++ MVVMProj/MVVMProj/Pages/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using MVVMProj.Models;
+﻿using Microsoft.Maui.Controls.Shapes;
+using MVVMProj.Models;
 using MVVMProj.PageModels;
 
 namespace MVVMProj.Pages
@@ -8,7 +9,34 @@ namespace MVVMProj.Pages
         public MainPage(MainPageModel model)
         {
             InitializeComponent();
-            BindingContext = model;
+            GenerateChessBoard();
         }
+
+        private void GenerateChessBoard()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                chessGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(50) });
+                chessGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(50) });
+            }
+
+            for (int row = 0; row < 8; row++)
+            {
+                for (int col = 0; col < 8; col++)
+                {
+                    var isBlack = (row + col) % 2 == 0;
+                    var boxView = new BoxView
+                    {
+                        Color = isBlack ? Colors.Black : Colors.White
+                    };
+
+                    Grid.SetRow(boxView, row);
+                    Grid.SetColumn(boxView, col);
+
+                    chessGrid.Children.Add(boxView);
+                }
+            }
+        }
+        
     }
 }
